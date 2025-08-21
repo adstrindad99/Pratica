0dde6aa766c0531051711100af12e4c6fb8815d8
adstrindad99 <adson.trindade99@outlook.com>
2025-08-20 21:23:43 -0300

    first commit

---
+++ src/Pratica1.java
@@ -0,0 +1,10 @@
+public class Pratica1 {
+    public static void main(String[] args) {
+        String nomeCompleto = "Adson Trindade dos Santos";
+        int idade = 25;
+        String dataDeNascimento = "25/09/1999";
+
+        System.out.println("Meu nome é: " + nomeCompleto);
+        System.out.println("Minha data de nascimento é: " + dataDeNascimento + " e tenho " + idade + " anos.");
+    }
+}
